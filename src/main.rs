e03f2120ee06c21b3923a154bcddf24b3ee00472
turtle-is <skull.2003@bk.ru>
2023-01-07 03:44:23 +0300

    Переделал серверную часть

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,51 @@
+use hello_cargo::ThreadPool;
+use std::fs;
 use std::io::prelude::*;
 use std::net::TcpListener;
 use std::net::TcpStream;
-use std::fs::File;
+use std::thread;
+use std::time::Duration;
 
+fn main() {
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let pool = ThreadPool::new(4);
 
+    for stream in listener.incoming().take(2) {
+        let stream = stream.unwrap();
 
-fn main(){
-    let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
-    for stream in listener.incoming(){
-        let stream : TcpStream = stream.unwrap();
-
-        println!("connection established!");
-        handle_connection(stream);
+        pool.execute(|| {
+            handle_connection(stream);
+        });
     }
 
+    println!("Shutting down.");
 }
 
-
-
-
-
-fn handle_connection(mut stream: TcpStream){
-    let mut buffer = [0; 512];
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 1024];
     stream.read(&mut buffer).unwrap();
 
-    println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
+    let get = b"GET / HTTP/1.1\r\n";
+    let sleep = b"GET /sleep HTTP/1.1\r\n";
 
-    let mut file = File::open("santa.html").unwrap();
-    let mut contents = String::new();
-    file.read_to_string(&mut contents).unwrap();
-
-
-    let response = format! {
-        "HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}",
-        contents.len(),
-        contents
+    let (status_line, filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK", "hello.html")
+    } else if buffer.starts_with(sleep) {
+        thread::sleep(Duration::from_secs(5));
+        ("HTTP/1.1 200 OK", "hello.html")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND", "404.html")
     };
 
-    stream.write(response.as_bytes()).unwrap();
+    let contents = fs::read_to_string(filename).unwrap();
+
+    let response = format!(
+        "{}\r\nContent-Length: {}\r\n\r\n{}",
+        status_line,
+        contents.len(),
+        contents
+    );
+
+    stream.write_all(response.as_bytes()).unwrap();
     stream.flush().unwrap();
-
 }
-
