b34ae591a833607ae933f6422871533f63b212e2
turtle-is <skull.2003@bk.ru>
2023-01-08 04:04:21 +0300

    переделал main файл, но это еще демо версия

--- src/main.rs
+++ src/main.rs
@@ -1,52 +1,104 @@
-use hello_cargo::ThreadPool;
-// use hello_cargo::Service;
-use std::fs;
-use std::io::prelude::*;
-use std::net::TcpListener;
-use std::net::TcpStream;
-use std::thread;
-use std::time::Duration;
-
-fn main() {
-    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
-    let pool = ThreadPool::new(4);
-
-    for stream in listener.incoming().take(2) {
-        let stream = stream.unwrap();
-
-        pool.execute(|| {
-            handle_connection(stream);
-        });
-    }
-
-    println!("Shutting down.");
+use std::{
+    collections::HashMap,
+    fs::File,
+    sync::{Arc,Mutex},
 }
 
-fn handle_connection(mut stream: TcpStream) {
-    let mut buffer = [0; 1024];
-    stream.read(&mut buffer).unwrap();
+use tide::Request;
 
-    let get = b"GET / HTTP/1.1\r\n";
-    let sleep = b"GET /sleep HTTP/1.1\r\n";
+#[derive(serde::Serializee, serde::Deserialize, Debug)]
+#[serde(rename_all = "snake_case")]
 
-    let (status_line, filename) = if buffer.starts_with(get) {
-        ("HTTP/1.1 200 OK", "hello.html")
-    } else if buffer.starts_with(sleep) {
-        thread::sleep(Duration::from_secs(5));
-        ("HTTP/1.1 200 OK", "hello.html")
-    } else {
-        ("HTTP/1.1 404 NOT FOUND", "404.html")
+enum Access{
+    Guest,
+    User,
+    Admin,
+}
+
+#[derive(serde::Deserialize)]
+struct User{
+    name: String,
+    access: Access,
+}
+
+#[derive(serde::Serializee, serde::Deserialize)]
+struct DataBase{
+    users: HashMap<String, Access>
+}
+
+#[tokio::main]
+
+async fn main() -> Result<(), std::io::Error>{
+    let version: &'static str = env!("CARGO_PKG_VERSION");
+
+    let database = match File::open("data.base"){
+        Ok(file) => serde_json::from_reader(file).map_err(|err|{
+            let err = std::io::Error::from(err);
+            std::io::Error::new(
+                err.kind(),
+                format!("Failed to read from database file. {err}"),
+            )
+        })?,
+        Err(err) if err.kind() = std::io::ErrorKind::NotFound =>{
+            eprintln!("Database file not found. Creating one.");
+
+            let file = File::create("data.base").map_err(|err|{
+                std::io::Error::new(err.kind(), format!("Failed to create database file. {err}"))
+            })?;
+            let database = DataBase{
+                users:HashMap::new();
+            };
+            serde_json::to_writer(file, &database).map_err(|err|{
+                let err = std::io::Error::from(err);
+                std::io::Error::new(
+                    err.kind(),
+                    format!("Failed to write to database file. {err}"),
+                )
+            })?;
+
+            database
+        }
+        Err(err)=>{
+            panic!("Failed to open database file. {err}");
+        }
     };
 
-    let contents = fs::read_to_string(filename).unwrap();
+    let state = Arc::new(Mutex::new(database));
 
-    let response = format!(
-        "{}\r\nContent-Length: {}\r\n\r\n{}",
-        status_line,
-        contents.len(),
-        contents
-    );
+    let mut app = tide::with_state(state);
+    app.at("/version").get(move |_| async move {Ok(serde_json::json!({ "version": version }))});
 
-    stream.write_all(response.as_bytes()).unwrap();
-    stream.flush().unwrap();
+    app.at("/add-user")
+        .put(|mut request: Request<Arc<Mutex<DataBase>>>>| async move {
+            let User { name, access } = request.body_json().await?;
+
+            eprintln!("Adding user {name} with {access:?}");
+
+            let state = request.state();
+            let mut guard = state.lock().unwrap();
+
+            guard.users.insert(name, version);
+
+            Ok(tide::StatusCode::Ok)
+        });
+
+        app.at("/get-user")
+            .get(|mut request: Request<Arc<Mutex<DataBase>>>| async move {
+                let name: String = request.body_json().await?;
+
+                let state = request.state();
+                let guard = state.lock().unwra();
+
+                eprintln!("Searching for user {name}");
+
+                match guard.users.get(&name){
+                    None => Err(tide::Error::from_str(
+                        tide::StatusCode::NotFound,
+                        format!("User {name} not found"),
+                    )),
+                    Some(access) => Ok(serde_json::json!({"access": access})),
+                }
+            });
+
+            app.listen("127.0.0.1:8080").await
 }
